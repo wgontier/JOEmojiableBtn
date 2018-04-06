6da86f529a2966cdf85e0981bc63376c19f9eb26
Jorge R Ovalle Z <jorge.ovalle@wizeline.com>
2018-04-05 20:36:38 -0500

    Add Swiftlint validation

--- Example/JOEmojiableBtn/ViewController.swift
+++ Example/JOEmojiableBtn/ViewController.swift
@@ -9,33 +9,31 @@
 import UIKit
 import JOEmojiableBtn
 
-
-class ViewController: UIViewController,JOEmojiableDelegate {
-    var labelInfo:UILabel!
+class ViewController: UIViewController, JOEmojiableDelegate {
+    var labelInfo: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let label           = UILabel(frame: CGRect(x: 0,y: 30,width: self.view.frame.width,height: 30))
-        label.text          = "Long tap in the buttons"
-        label.textColor     = UIColor.lightGray
+        let label = UILabel(frame: CGRect(x: 0, y: 30, width: self.view.frame.width, height: 30))
+        label.text = "Long tap in the buttons"
+        label.textColor = UIColor.lightGray
         label.textAlignment = .center
         self.view.addSubview(label)
         
-        labelInfo           = UILabel(frame: CGRect(x: 0,y: 60,width: self.view.frame.width,height: 30))
-        labelInfo.text          = ""
-        labelInfo.textColor     = UIColor(red:0.27, green:0.69, blue:0.67, alpha:1)
+        labelInfo = UILabel(frame: CGRect(x: 0, y: 60, width: self.view.frame.width, height: 30))
+        labelInfo.text = ""
+        labelInfo.textColor = UIColor(red: 0.27, green: 0.69, blue: 0.67, alpha: 1)
         labelInfo.textAlignment = .center
         self.view.addSubview(labelInfo)
         
-        
-        let btn                = JOEmojiableBtn(frame: CGRect(x: 40,y: 200,width: 100,height: 50))
-        btn.delegate           = self
-        btn.titleLabel?.font   = UIFont.systemFont(ofSize: 11)
-        btn.backgroundColor    = UIColor(red:0.27, green:0.69, blue:0.67, alpha:1)
+        let btn = JOEmojiableBtn(frame: CGRect(x: 40, y: 200, width: 100, height: 50))
+        btn.delegate = self
+        btn.titleLabel?.font = UIFont.systemFont(ofSize: 11)
+        btn.backgroundColor = UIColor(red: 0.27, green: 0.69, blue: 0.67, alpha: 1)
         btn.layer.cornerRadius = 25
         btn.setTitle("Long-tap me!", for: [.normal])
-        btn.dataset            = [
+        btn.dataset = [
             JOEmojiableOption(image: "img_1", name: "dislike"),
             JOEmojiableOption(image: "img_2", name: "broken"),
             JOEmojiableOption(image: "img_3", name: "he he"),
@@ -45,15 +43,14 @@ class ViewController: UIViewController,JOEmojiableDelegate {
         ]
         self.view.addSubview(btn)
         
-
-        let config              = JOEmojiableConfig(spacing: 2, size: 30, minSize: 34, maxSize: 45, s_options_selector: 30)
-        let btn2                = JOEmojiableBtn(frame: CGRect(x: 40,y: 300,width: 100,height: 50), config: config)
-        btn2.delegate           = self
-        btn2.backgroundColor    = UIColor(red:0.27, green:0.69, blue:0.67, alpha:1)
+        let config = JOEmojiableConfig(spacing: 2, size: 30, minSize: 34, maxSize: 45, s_options_selector: 30)
+        let btn2 = JOEmojiableBtn(frame: CGRect(x: 40, y: 300, width: 100, height: 50), config: config)
+        btn2.delegate = self
+        btn2.backgroundColor = UIColor(red: 0.27, green: 0.69, blue: 0.67, alpha: 1)
         btn2.layer.cornerRadius = 25
-        btn2.titleLabel?.font   = UIFont.systemFont(ofSize: 11)
+        btn2.titleLabel?.font = UIFont.systemFont(ofSize: 11)
         btn2.setTitle("Long-tap me!", for: [.normal])
-        btn2.dataset            = [
+        btn2.dataset = [
             JOEmojiableOption(image: "img_1", name: "dislike"),
             JOEmojiableOption(image: "img_2", name: "broken"),
             JOEmojiableOption(image: "img_3", name: "he he"),
@@ -63,7 +60,6 @@ class ViewController: UIViewController,JOEmojiableDelegate {
             JOEmojiableOption(image: "img_4", name: "ooh")
         ]
         self.view.addSubview(btn2)
-        
         
     }
     
@@ -86,4 +82,3 @@ class ViewController: UIViewController,JOEmojiableDelegate {
         super.didReceiveMemoryWarning()
     }
 }
-

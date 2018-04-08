813b45e0ff073f745d0d9dc7e73e1f9e67606f15
Jorge R Ovalle Z <jorge.ovalle@wizeline.com>
2018-04-08 16:58:46 -0500

    Add documentation for `JOEmojiableOption

--- Pod/Classes/JOEmojiableOption.swift
+++ Pod/Classes/JOEmojiableOption.swift
@@ -8,10 +8,20 @@
 
 import Foundation
 
+/// A type that represents the option of a selector.
 public struct JOEmojiableOption {
-    var image: String
-    var name: String
     
+    /// The image will be used for the option represented. (This image should be added to the bundle).
+    let image: String
+    
+    /// The name of the option represented.
+    let name: String
+    
+    /// Creates an instance of `JOEmojiableOption`.
+    ///
+    /// - Parameters:
+    ///   - image: The image will be used for the option represented. (This image should be added to the bundle).
+    ///   - name: The name of the option represented.
     public init(image: String, name: String) {
         self.image = image
         self.name  = name

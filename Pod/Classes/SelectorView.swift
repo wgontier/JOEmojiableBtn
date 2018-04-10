0c90cc5df0380f3e455d6c4f799acf1753d147a2
Jorge R Ovalle Z <jorge.ovalle@wizeline.com>
2018-04-09 23:23:16 -0500

    Add documentation for `SelectorView` and `SelectorViewDelegate`

--- Pod/Classes/SelectorView.swift
+++ Pod/Classes/SelectorView.swift
@@ -8,19 +8,41 @@
 
 import UIKit
 
+/// Describes a type that is informed of events occurring within a `SelectorView`.
 public protocol SelectorViewDelegate: class {
+    
+    /// This function is dispatched when `touchesMoved` is triggered in `SelectorView`.
+    ///
+    /// - Parameter point: CGPoint where the finger moved.
     func movedTo(_ point: CGPoint)
+    
+    /// This function is dispatched when `touchesEnded` is triggered in `SelectorView`.
+    ///
+    /// - Parameter point: CGPoint where the finger move ended.
     func endTouch(_ point: CGPoint)
 }
 
+/// A type that represents the SelectorView where options are shown.
 open class SelectorView: UIView {
+    
+    /// The object that acts as the delegate of the `SelectorView`.
     weak var delegate: SelectorViewDelegate?
 
+    /// Sent to the gesture recognizer when one or more fingers move in the associated view.
+    ///
+    /// - Parameters:
+    ///   - touches: A set of UITouch instances in the event represented by event that represent touches in the UITouchPhaseMoved phase.
+    ///   - event: A UIEvent object representing the event to which the touches belong.
     open override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let location = (touches.first?.location(in: self)) else { return }
         delegate?.movedTo(location)
     }
 
+    /// Sent to the gesture recognizer when one or more fingers lift from the associated view.
+    ///
+    /// - Parameters:
+    ///   - touches: A set of UITouch instances in the event represented by event that represent the touches in the UITouchPhaseEnded phase.
+    ///   - event: A UIEvent object representing the event to which the touches belong.
     open override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let location = (touches.first?.location(in: self)) else { return }
         delegate?.endTouch(location)
